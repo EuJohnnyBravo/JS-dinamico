02da2a8b3917d7e6a17e4dbaf9a334591ba47a7c
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-10-07 19:13:58 -0300

    desafio-1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,65 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const nome = document.querySelector<HTMLInputElement>("#nome");
+const email = document.querySelector<HTMLInputElement>("#email");
+const idade = document.querySelector<HTMLInputElement>("#idade");
+const cpf = document.querySelector<HTMLInputElement>("#cpf");
+const dados = document.querySelector<HTMLElement>("#dados");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const button = document.querySelector("button");
+if (button) {
+  button.addEventListener("click", () => {
+    if (email && nome && idade && cpf && dados) {
+      const valorNome = nome.value;
+      const valorEmail = email.value;
+      const valorIdade = Number(idade.value);
+      const valorCpf = cpf.value;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+      if (validarDados(valorNome, valorEmail, valorIdade, valorCpf)) {
+        dados.innerText = `Olá ${valorNome}! Seus dados estão corretos! :D`;
+      }
+    }
+  });
+}
+
+function validarEmail(email: string): boolean {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
+
+function validarCPF(cpf: string): boolean {
+  const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+  return regex.test(cpf);
+}
+
+function validarIdade(idade: number): boolean {
+  if (idade >= 0) {
+    return true;
+  }
+  return false;
+}
+
+function validarDados(
+  valorNome: string,
+  valorEmail: string,
+  valorIdade: number,
+  valorCpf: string
+): boolean {
+  if (dados) {
+    if (!valorNome) {
+      dados.innerText = `Nome invalido`;
+      return false;
+    }
+    if (!validarEmail(valorEmail)) {
+      dados.innerText = `Email com formato invalido`;
+      return false;
+    }
+    if (!validarIdade(valorIdade)) {
+      dados.innerText = `Idade menor que 0`;
+      return false;
+    }
+    if (!validarCPF(valorCpf)) {
+      dados.innerText = `CPF invalido`;
+      return false;
+    }
+  }
+  return true;
+}
