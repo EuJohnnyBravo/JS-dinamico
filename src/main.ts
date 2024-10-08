5bb09b9c597cbd807ee5f7ad09cd564ce148e4d0
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-10-08 19:57:16 -0300

    desafio-2

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,61 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const lampadas = ["./desligada.jpg", "./ligada.jpg", "./quebrada.jpg"];
+const OnOff = ["./Off.png", "./On.png"];
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const imagemLampada = document.querySelector<HTMLImageElement>("#lamp");
+const imagemSwitch = document.querySelector<HTMLImageElement>("#switch");
+const interruptor = document.querySelector<HTMLElement>("button");
+let lampadaQuebrada: boolean = false;
+let ligada: boolean = false;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function vaiQuebrar(): boolean {
+  return Math.random() <= 0.05;
+}
+
+function ligaDesliga(isLigada: boolean): boolean {
+  const valor = isLigada;
+  if (valor) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+if (interruptor && imagemLampada && imagemSwitch) {
+  imagemLampada.src = lampadas[0];
+  imagemSwitch.src = OnOff[0];
+  interruptor.addEventListener("click", () => {
+    ligada = ligaDesliga(ligada);
+    if (!lampadaQuebrada) {
+      lampadaQuebrada = vaiQuebrar();
+      if (ligada) {
+        imagemLampada.src = lampadas[1];
+        imagemSwitch.src = OnOff[1];
+      } else {
+        imagemLampada.src = lampadas[0];
+        imagemSwitch.src = OnOff[0];
+      }
+    } else {
+      imagemLampada.src = lampadas[2];
+    }
+  });
+  interruptor.addEventListener("click", () => {
+    if (ligada) {
+      imagemSwitch.src = OnOff[1];
+    } else {
+      imagemSwitch.src = OnOff[0];
+    }
+  });
+
+  imagemLampada.addEventListener("click", () => {
+    if (!ligada) {
+      lampadaQuebrada = false;
+      ligada = false;
+      imagemLampada.src = lampadas[0];
+      imagemSwitch.src = OnOff[0];
+    } else {
+      alert(
+        "Brother, tu não é gato pra ter sete vidas, hein! Desliga esse interruptor agora, que o choque vem e não avisa!"
+      );
+    }
+  });
+}
