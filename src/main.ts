e55324a784eb37d19bacb70aff63516c56f42618
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-10-09 21:50:37 -0300

    desafio 3

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,45 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+console.log("to do list!");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const KEY = "@LISTA";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const input = document.querySelector<HTMLInputElement>("#inputTexto");
+const botao = document.querySelector<HTMLInputElement>("#salvar");
+const lista = document.querySelector<HTMLInputElement>("#todoList");
+let tarefas: string[] = [];
+const tarefasArmazenadasString = localStorage.getItem(KEY);
+if (tarefasArmazenadasString) {
+  tarefas = JSON.parse(tarefasArmazenadasString);
+  mostrarTarefas();
+}
+
+function salvarTarefa() {
+  if (input) {
+    const tarefa = input.value;
+    console.log(input);
+    tarefas.push(tarefa);
+    localStorage.setItem(KEY, JSON.stringify(tarefas));
+  }
+}
+
+function mostrarTarefas() {
+  if (lista) {
+    lista.innerHTML = "";
+  }
+  const ul = document.createElement("ul");
+  tarefas.forEach((t) => {
+    const li = document.createElement("li");
+    li.innerText = t;
+    ul.appendChild(li);
+  });
+  lista?.appendChild(ul);
+}
+
+if (botao) {
+  botao.addEventListener("click", () => {
+    salvarTarefa();
+    if (input) {
+      input.value = "";
+      mostrarTarefas();
+    }
+  });
+}
